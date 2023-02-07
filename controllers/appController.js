be0623b22037092426aad8a53307d1cf59dc0666
Risvi-Tareq <ivsirqerat@gmail.com>
2023-02-06 20:57:50 -0800

    set up structure

---
+++ controllers/appController.js
@@ -0,0 +1,113 @@
+const { Application, User } = require('../models');
+
+module.exports = {
+  getApplications(req, res) {
+    Application.find()
+      .then((applications) => res.json(applications))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleApplication(req, res) {
+    Application.findOne({ _id: req.params.applicationId })
+      .then((application) =>
+        !application
+          ? res.status(404).json({ message: 'No application with that ID' })
+          : res.json(application)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // TODO: Add comments to the functionality of the createApplication method using the Application schema
+  // creates a new application  
+  createApplication(req, res) {
+    Application.create(req.body)
+      .then((application) => {
+        // if there is a user under the id in which the application is created then update that users data set to include the new application
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { applications: application._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        // if there is no user, then create an application with no user
+        !user
+          ? res.status(404).json({
+              message: 'Application created, but found no user with that ID',
+            })
+          : res.json('Created the application 🎉')
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // TODO: Add comments to the functionality of the updateApplication method
+// updates an application if id is found
+  updateApplication(req, res) {
+    Application.findOneAndUpdate(
+      { _id: req.params.applicationId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((application) =>
+        !application
+          ? res.status(404).json({ message: 'No application with this id!' })
+          : res.json(application)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // TODO: Add comments to the functionality of the deleteApplication method
+  // deletes an application if id is found
+  deleteApplication(req, res) {
+    Application.findOneAndRemove({ _id: req.params.applicationId })
+      .then((application) =>
+        !application
+          ? res.status(404).json({ message: 'No application with this id!' })
+          : User.findOneAndUpdate(
+              { applications: req.params.applicationId },
+              { $pull: { applications: req.params.applicationId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: 'Application created but no user with this id!',
+            })
+          : res.json({ message: 'Application successfully deleted!' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // TODO: Add comments to the functionality of the addTag method
+  // updates data of the application to include a new tag if the application exists
+  addTag(req, res) {
+    Application.findOneAndUpdate(
+      { _id: req.params.applicationId },
+      { $addToSet: { tags: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((application) =>
+        !application
+          ? res.status(404).json({ message: 'No application with this id!' })
+          : res.json(application)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // TODO: Add comments to the functionality of the addTag method
+  // deletes tag from application if found
+  removeTag(req, res) {
+    Application.findOneAndUpdate(
+      { _id: req.params.applicationId },
+      { $pull: { tags: { tagId: req.params.tagId } } },
+      { runValidators: true, new: true }
+    )
+      .then((application) =>
+        !application
+          ? res.status(404).json({ message: 'No application with this id!' })
+          : res.json(application)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
